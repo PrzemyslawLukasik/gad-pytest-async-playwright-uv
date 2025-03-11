c9948ac683dbf2e9cb99f3725ad3b14950773e5e
Przemyslaw Lukasik <przemyslaw.lukasik@vidaa.com>
2025-03-11 15:14:36 +0100

    ADD: test_one.py module

---
+++ src/tests/test_one.py
@@ -0,0 +1,22 @@
+import pytest
+import logging
+
+from playwright.async_api import Page
+from src.pages.main_page import MainPage
+from src.pages.practice_page import PracticePage
+
+my_logger = logging.getLogger(__name__)
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_open_practice_page(page: Page) -> None:
+    main_page = MainPage(page)
+    practice_page = PracticePage(page)
+    await main_page.visit()
+    await main_page.click_on_practice_pages_btn()
+    await practice_page.is_practice_page_open()
+
+
+    my_logger.warning('WARNING')
+    my_logger.info(f"pytest.env: {pytest.env}")
+    assert 1 == 2
